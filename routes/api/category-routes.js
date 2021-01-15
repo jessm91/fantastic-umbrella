a9bcea7dcf1e709a934e64d6ba55360d940ec2bd
Jessica Mangiameli <jmangiam@asu.edu>
2021-01-15 08:47:32 -0600

    fixed post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,12 +54,12 @@ router.post('/', (req, res) => {
   // create a new category
   Category.create({
     category_name: req.body.category_name
-  })
-  .then(dbCatData => res.json(dbCataData))
-  .catch(err => {
+})
+.then(dbCatData => res.json(dbCatData))
+.catch(err => {
     console.log(err);
     res.status(500).json(err);
-  });
+});
 });
 
 router.put('/:id', (req, res) => {
